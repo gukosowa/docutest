ba6f8a07afc1eca5b2e66ecdfe9804b412af2350
gukosowa <marcus.lechner@gmail.com>
2022-02-05 11:11:15 +0100

    todo

--- conf.py
+++ conf.py
@@ -1,52 +1,161 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -*- coding: utf-8 -*-
 
-# -- Path setup --------------------------------------------------------------
+import sphinx
+import sphinx_rtd_theme
+import sys
+import os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# -- General configuration ------------------------------------------------
 
+needs_sphinx = "1.3"
 
-# -- Project information -----------------------------------------------------
-
-project = 'hs'
-copyright = '2022, markus'
-author = 'markus'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Sphinx extension module names and templates location
+sys.path.append(os.path.abspath("_extensions"))
 extensions = [
+    "notfound.extension",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# Warning when the Sphinx Tabs extension is used with unknown
+# builders (like the dummy builder) - as it doesn't cause errors,
+# we can ignore this so we still can treat other warnings as errors.
+sphinx_tabs_nowarn = True
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# Custom 4O4 page HTML template.
+# https://github.com/readthedocs/sphinx-notfound-page
+notfound_context = {
+    "title": "Page not found",
+    "body": """
+        <h1>Page not found</h1>
+        <p>
+            Sorry, we couldn't find that page. It may have been renamed or removed
+            in the version of the documentation you're currently browsing.
+        </p>
+        <p>
+            If you're currently browsing the
+            <em>latest</em> version of the documentation, try browsing the
+            <a href="/en/stable/"><em>stable</em> version of the documentation</a>.
+        </p>
+        <p>
+            Alternatively, use the
+            <a href="#" onclick="$('#rtd-search-form [name=\\'q\\']').focus()">Search docs</a>
+            box on the left or <a href="/">go to the homepage</a>.
+        </p>
+    """,
+}
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+# Don't add `/en/latest` prefix during local development.
+# This makes it easier to test the custom 404 page by loading `/404.html`
+# on a local web server.
+if not on_rtd:
+    notfound_urls_prefix = ''
+
+# Specify the site name for the Open Graph extension.
+ogp_site_name = "hstd"
+
+templates_path = ["_templates"]
+
+# You can specify multiple suffix as a list of string: ['.rst', '.md']
+source_suffix = ".rst"
+source_encoding = "utf-8-sig"
+
+# The master toctree document
+master_doc = "index"
+
+# General information about the project
+project = "hstd"
+copyright = (
+    "hs"
+)
+author = "markus"
+
+# Version info for the project, acts as replacement for |version| and |release|
+# The short X.Y version
+version = os.getenv("READTHEDOCS_VERSION", "latest")
+# The full version, including alpha/beta/rc tags
+release = version
+
+# Parse Sphinx tags passed from RTD via environment
+env_tags = os.getenv("SPHINX_TAGS")
+if env_tags is not None:
+    for tag in env_tags.split(","):
+        print("Adding Sphinx tag: %s" % tag.strip())
+        tags.add(tag.strip())  # noqa: F821
 
 
-# -- Options for HTML output -------------------------------------------------
+exclude_patterns = ["_build"]
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+smartquotes = False
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# Pygments (syntax highlighting) style to use
+pygments_style = "sphinx"
+highlight_language = "js"
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if on_rtd:
+    using_rtd_theme = True
+
+# Theme options
+html_theme_options = {
+    # if we have a html_logo below, this shows /only/ the logo with no title text
+    "logo_only": True,
+    # Collapse navigation (False makes it tree-like)
+    "collapse_navigation": False,
+}
+
+html_title = "hstd"
+
+# VCS options: https://docs.readthedocs.io/en/latest/vcs.html#github
+html_context = {
+    "display_github": "jj",  # Integrate GitHub
+    "github_user": "gukosowa",  # Username
+    "github_repo": "docutest",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "/",  # Path in the checkout to the docs root
+}
+
+html_logo = "img/docs_logo.png"
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ["_static"]
+
+html_extra_path = ["robots.txt"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (e.g. https://...)
+html_css_files = [
+    'css/algolia.css',
+    'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css',
+    "css/custom.css",
+]
+
+if not on_rtd:
+    html_css_files.append("css/dev.css")
+
+html_js_files = [
+    "js/custom.js",
+    ('https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js', {'defer': 'defer'}),
+    ('js/algolia.js', {'defer': 'defer'})
+]
+
+# Output file base name for HTML help builder
+htmlhelp_basename = "Documentationdoc"
+
+# -- Options for reStructuredText parser ----------------------------------
+
+# Enable directives that insert the contents of external files
+file_insertion_enabled = False
+
+# -- Options for linkcheck builder ----------------------------------------
+
+# disable checking urls with about.html#this_part_of_page anchors
+linkcheck_anchors = False
+
+linkcheck_timeout = 10
+
+cwd = os.getcwd()
